8351aedecd88c05cd69560db5ffd2c6636106546
Rakesh Perala <raakeshperala16@gmail.com>
2026-03-29 17:42:44 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -1,19 +1,4 @@
-#################################################
-# Provider
-#################################################
-terraform {
-  required_version = ">= 1.4.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.100"
-    }
-  }
-}
 
-provider "aws" {
-  region = "ap-south-2"
-}
 
 #################################################
 # Data Sources
